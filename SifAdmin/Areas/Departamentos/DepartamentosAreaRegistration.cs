86d3d008e5804444df6b0bd721adbe589d8460af
Jose Castro <drakonc@users.noreply.github.com>
2019-07-08 16:11:39 -0500

    Agregar archivos de proyecto.

---
+++ SifAdmin/Areas/Departamentos/DepartamentosAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace SifAdmin.Areas.Departamentos
+{
+    public class DepartamentosAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Departamentos";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Departamentos_default",
+                "Departamentos/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ SifAdmin/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SifAdmin.MvcApplication" Language="C#" %>
